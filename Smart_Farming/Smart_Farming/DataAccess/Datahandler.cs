6029cec8859a1fc82ffe24c084f6811be4e51848
Juan-Pierre Prinsloo <576598@student.belgiumcampus.ac.za>
2021-06-13 16:05:51 +0200

    Datahandler basic implementation

--- Smart_Farming/Smart_Farming/DataAccess/Datahandler.cs
+++ Smart_Farming/Smart_Farming/DataAccess/Datahandler.cs
@@ -1,10 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Data;
 
 namespace Smart_Farming.DataAccess
 {
     class Datahandler
     {
+        private string connectionString = @"";
+
+        public static DataTable GetDataTable(string Query)
+        {
+            DataTable data = new DataTable();
+
+            //database stuff goes here wheewe figure it out later
+
+            //using (MySqlConnection conn = new MySqlConnection(connStr))
+            //{
+            //    try
+            //    {
+            //        conn.Open();
+            //        MySqlDataAdapter adapter = new MySqlDataAdapter(Query, conn);
+            //        adapter.Fill(data);
+            //    }
+            //    catch (Exception e)
+            //    {
+            //        //Console.WriteLine(e.StackTrace);
+            //        MessageBox.Show(string.Format("Error: {0}\n\rSQL: {1}", e.Message, Query), "SQL ERROR");
+            //    }
+            //}
+
+            return data;
+        }
+
     }
 }
