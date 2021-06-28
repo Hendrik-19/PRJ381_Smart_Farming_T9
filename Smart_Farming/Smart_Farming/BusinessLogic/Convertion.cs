204b1f314b8a9ace1a4cda5a78173d8cb4081c2c
Hendrik-19 <576326@student.belgiumcampus.ac.za>
2021-06-28 17:55:44 +0200

    Added a class that can convert datatables to lists

---
+++ Smart_Farming/Smart_Farming/BusinessLogic/Convertion.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection; // used to convert
+using System.Data;
+
+namespace Smart_Farming.BusinessLogic
+{
+    public class Convertion
+    {
+        // this class is used to convert a datatable to a list
+
+        public List<T> ConvertDataTable<T>(DataTable table)
+        {
+            List<T> list = new List<T>();
+            foreach (DataRow row in table.Rows)
+            {
+                T item = GetItem<T>(row);
+                list.Add(item);
+            }
+            return list;
+        } 
+
+        private T GetItem<T>(DataRow row)
+        {
+            Type temp = typeof(T);
+            T obj = Activator.CreateInstance<T>();
+
+            foreach (DataColumn column in row.Table.Columns)
+            {
+                foreach (PropertyInfo propertyInfo in temp.GetProperties())
+                {
+                    if (propertyInfo.Name == column.ColumnName)
+                    {
+                        propertyInfo.SetValue(obj, row[column.ColumnName], null);
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+            }
+
+            return obj;
+        }
+    }
+}
