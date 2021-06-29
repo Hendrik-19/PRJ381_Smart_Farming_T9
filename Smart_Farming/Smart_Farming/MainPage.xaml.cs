a962596fd5a9e2bf638188d74015509cd1e9ff3f
Hendrik-19 <576326@student.belgiumcampus.ac.za>
2021-06-29 14:49:38 +0200

    added the location services to the app

--- Smart_Farming/Smart_Farming/MainPage.xaml.cs
+++ Smart_Farming/Smart_Farming/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Essentials; // api that allows location services
 
 namespace Smart_Farming
 {
@@ -13,6 +14,35 @@ namespace Smart_Farming
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var location = await Geolocation.GetLastKnownLocationAsync(); // this gets the last location that is saved in the device cache
+                if (location == null) // thios checks of a location saved in the cache, if it is empty then it will request a location
+                {
+                    location = await Geolocation.GetLocationAsync(new GeolocationRequest
+                    {
+                        DesiredAccuracy = GeolocationAccuracy.Medium,
+                        Timeout = TimeSpan.FromSeconds(30) // if the app takes more than 30 seconds to get a location it will go to the catch 
+                    });
+                }
+
+                if (location == null)
+                {
+                    LabelLocation.Text = "No GPS"; // if the result is still empty it will give a message showing that there is no location
+                }
+                else
+                {
+                    LabelLocation.Text = "You have a location"; // need to display info but this will probably be where we load the next page
+                }
+            }
+            catch (Exception)
+            {
+                //TODO: appropriate error message for something that went wrong
+            }
         }
     }
 }
