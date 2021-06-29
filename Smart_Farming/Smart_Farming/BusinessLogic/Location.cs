254d802c279a2f6eaf7de388b1c1848deb5bb6f8
Hendrik-19 <576326@student.belgiumcampus.ac.za>
2021-06-29 14:54:29 +0200

    added some comments to the location class

--- Smart_Farming/Smart_Farming/BusinessLogic/Location.cs
+++ Smart_Farming/Smart_Farming/BusinessLogic/Location.cs
@@ -6,6 +6,7 @@ namespace Smart_Farming.BusinessLogic
 {
     class Location
     {
-        // Location services goes here
+        // here the manilulation of the results from the api will be manipulated and sent to the database so that it can retrieve the apprpriate results
+        // remeber to get the data from the form and send it here.
     }
 }
