7b7a2aa04433aaac90e60fcf657674f235084531
Hendrik-19 <576326@student.belgiumcampus.ac.za>
2021-06-29 14:18:25 +0200

    added the needed userpermissions for location services

--- Smart_Farming/Smart_Farming.Android/Properties/AssemblyInfo.cs
+++ Smart_Farming/Smart_Farming.Android/Properties/AssemblyInfo.cs
@@ -27,4 +27,9 @@ using Android.App;
 
 // Add some common permissions, these can be removed if not needed
 [assembly: UsesPermission(Android.Manifest.Permission.Internet)]
-[assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage)]
+[assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage)] // these permisions are added so that the app can use location services line 30 to 35
+[assembly: UsesPermission(Android.Manifest.Permission.AccessCoarseLocation)]
+[assembly: UsesPermission(Android.Manifest.Permission.AccessFineLocation)]
+[assembly: UsesFeature("android.hardware.location", Required = false)]
+[assembly: UsesFeature("android.hardware.location.gps", Required = false)]
+[assembly: UsesFeature("android.hardware.location.network", Required = false)]
