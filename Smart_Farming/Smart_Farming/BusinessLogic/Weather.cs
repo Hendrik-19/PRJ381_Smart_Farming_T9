e6ee08acbcb6d49b91986a095824c2d37c4a3c6d
Hendrik-19 <576326@student.belgiumcampus.ac.za>
2021-07-31 15:03:03 +0200

    added a class called weather to get the weather information from the api

---
+++ Smart_Farming/Smart_Farming/BusinessLogic/Weather.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Smart_Farming.BusinessLogic
+{
+    public class Weather
+    {
+        int month;
+        double temperatureMin, temperatureMax, totalPrecipitation, cloudCover, sunshineHours;
+
+        public int Month { get => month; set => month = value; }
+        public double TemperatureMin { get => temperatureMin; set => temperatureMin = value; }
+        public double TemperatureMax { get => temperatureMax; set => temperatureMax = value; }
+        public double TotalPrecipitation { get => totalPrecipitation; set => totalPrecipitation = value; }
+        public double CloudCover { get => cloudCover; set => cloudCover = value; }
+        public double SunshineHours { get => sunshineHours; set => sunshineHours = value; }
+
+        public Weather(int month, double temperatureMin, double temperatureMax, double totalPrecipitation, double cloudCover, double sunshineHours)
+        {
+            this.month = month;
+            this.temperatureMin = temperatureMin;
+            this.temperatureMax = temperatureMax;
+            this.totalPrecipitation = totalPrecipitation;
+            this.cloudCover = cloudCover;
+            this.sunshineHours = sunshineHours;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Weather weather &&
+                   month == weather.month &&
+                   temperatureMin == weather.temperatureMin &&
+                   temperatureMax == weather.temperatureMax &&
+                   totalPrecipitation == weather.totalPrecipitation &&
+                   cloudCover == weather.cloudCover &&
+                   sunshineHours == weather.sunshineHours;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = 183572641;
+            hashCode = hashCode * -1521134295 + month.GetHashCode();
+            hashCode = hashCode * -1521134295 + temperatureMin.GetHashCode();
+            hashCode = hashCode * -1521134295 + temperatureMax.GetHashCode();
+            hashCode = hashCode * -1521134295 + totalPrecipitation.GetHashCode();
+            hashCode = hashCode * -1521134295 + cloudCover.GetHashCode();
+            hashCode = hashCode * -1521134295 + sunshineHours.GetHashCode();
+            return hashCode;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
