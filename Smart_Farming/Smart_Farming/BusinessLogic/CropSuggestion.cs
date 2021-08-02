85a289d63805fced8981a892d87cd9425258058c
Juan-Pierre Prinsloo <576598@student.belgiumcampus.ac.za>
2021-08-02 11:05:29 +0200

    Added basic logic to fill crop list. NOTE: need to implement datatable -> list conversion and fix query as needed

--- Smart_Farming/Smart_Farming/BusinessLogic/CropSuggestion.cs
+++ Smart_Farming/Smart_Farming/BusinessLogic/CropSuggestion.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Smart_Farming.DataAccess;
 
 namespace Smart_Farming.BusinessLogic
 {
@@ -11,11 +12,16 @@ namespace Smart_Farming.BusinessLogic
          * 2: Return the crop list to be displayed to user
          */
 
-        public List<Crop> getCrops(Location loc)
+        public List<Crop> getCrops(Location loc) //TODO: Edit query as needed and fix list filling logic
         {
+            Datahandler handler = new Datahandler();
             List<Crop> crops = new List<Crop>();
+            //example query, could change if database gets changed. Could be incorrect, I'm no DBA
+            string query = $"SELECT * FROM Crops JOIN Climate ON Climate.Climate_ID = Crops.Climate_ID WHERE Crops.Climate_ID = {loc.ClimateID}";
 
             //logic
+            //Execute query to fill crops.
+            //crops.AddRange(handler.getData(query)); //TODO: Implement datatable to list conversion
 
             return crops;
         }
