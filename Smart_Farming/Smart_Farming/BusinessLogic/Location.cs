07caae4d1c74155749cac8736613bfefae2206e3
Juan-Pierre Prinsloo <576598@student.belgiumcampus.ac.za>
2021-08-02 10:45:10 +0200

    Made changes to location class based on new use case

--- Smart_Farming/Smart_Farming/BusinessLogic/Location.cs
+++ Smart_Farming/Smart_Farming/BusinessLogic/Location.cs
@@ -9,7 +9,21 @@ namespace Smart_Farming.BusinessLogic
         // here the manilulation of the results from the api will be manipulated and sent to the database so that it can retrieve the apprpriate results
         // remeber to get the data from the form and send it here.
 
-        public void getResults(double lat, double lon)
+        int climateID;
+        double latidue, longitude, avgMaxTemp, avgMinTemp, avgPercipitation;
+
+        public Location()
+        {
+        }
+
+        public int ClimateID { get => climateID; set => climateID = value; }
+        public double Latidue { get => latidue; set => latidue = value; }
+        public double Longitude { get => longitude; set => longitude = value; }
+        public double AvgMaxTemp { get => avgMaxTemp; set => avgMaxTemp = value; }
+        public double AvgMinTemp { get => avgMinTemp; set => avgMinTemp = value; }
+        public double AvgPercipitation { get => avgPercipitation; set => avgPercipitation = value; }
+
+        public void getResults(int climateId)
         {
             // stuff to be done
         }
