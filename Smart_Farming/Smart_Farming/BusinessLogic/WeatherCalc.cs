9b877e0bff657d6719efb92eaa4529cc2f0731da
Juan-Pierre Prinsloo <576598@student.belgiumcampus.ac.za>
2021-08-02 10:45:21 +0200

    Added weather calc class

---
+++ Smart_Farming/Smart_Farming/BusinessLogic/WeatherCalc.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Smart_Farming.BusinessLogic
+{
+    class WeatherCalc
+    {
+        /*
+         * 1:Use list of 12 months data recieved from API to calculate avg values for location
+         * 2:Return calculated avg values to location
+         */
+
+        public double getAvgMaxTemp(Location loc)
+        {
+            double locMaxAvg = 0;
+
+            //logic
+
+            return locMaxAvg;
+        }
+
+        public double getAvgMinTemp(Location loc)
+        {
+            double locMinAvg = 0;
+
+            //logic
+
+            return locMinAvg;
+        }
+
+        public double getAvgPrecipitation(Location loc)
+        {
+            double locPrecAvg = 0;
+
+            //logic
+
+            return locPrecAvg;
+        }
+    }
+}
