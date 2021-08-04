00f363384a575950e4c4e851e42bfb70acb54a09
Hendrik-19 <576326@student.belgiumcampus.ac.za>
2021-08-04 14:08:07 +0200

    added some code in the climate calc class

--- Smart_Farming/Smart_Farming/BusinessLogic/ClimateCalc.cs
+++ Smart_Farming/Smart_Farming/BusinessLogic/ClimateCalc.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using Smart_Farming.DataAccess;
+using System.Data; //  needed to create a datatable
 
 namespace Smart_Farming.BusinessLogic
 {
@@ -14,19 +15,26 @@ namespace Smart_Farming.BusinessLogic
          * 4: Assign detected climate to location
          */
 
+        #region Functionality
         public int assignClimate(Location loc)
         {
-            Datahandler handler = new Datahandler();
-            List<Climates> climateList = new List<Climates>();
             int LocationClimateID = 0;
             string query = $"";//TODO:complete query
 
-            //climateList.AddRange(handler.getData(query)); //TODO:Implement Datatable to list conversion
-            
+            Datahandler handler = new Datahandler();
+            Convertion convert = new Convertion();
+
+            DataTable temp = new DataTable();
+            temp = handler.getData(query);
+
+            List<Climates> climateList = new List<Climates>();
+            climateList  = convert.ConvertDataTable<Climates>(temp);
+
             //logic //TODO: complete logic
 
 
             return LocationClimateID;
         }
+        #endregion
     }
 }
