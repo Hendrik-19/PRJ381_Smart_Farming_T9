1aeac0902ae26fc785019c67c2da086b633493f8
Juan-Pierre Prinsloo <576598@student.belgiumcampus.ac.za>
2021-10-23 13:38:39 +0200

    ensures that the user can't loop through suggestions if there is only one crop suggestion

--- Smart_Farming/Smart_Farming/Page1.xaml.cs
+++ Smart_Farming/Smart_Farming/Page1.xaml.cs
@@ -23,43 +23,59 @@ namespace Smart_Farming
         {
             croplist = crops;
             Img = croplist[counter].CropImage;
-            lblCName.Text = "Crop name: " + croplist[counter].CropName.ToString();
-            lblSTime.Text = "Sow time: " + croplist[counter].SowTime.ToString();
-            lblHTime.Text = "Harvest time: " + croplist[counter].HarvestTime.ToString() + "";//add appropriate info
-            lblIAmmount.Text = "Irrigation amount needed: " + croplist[counter].IrrigationAmount.ToString() + "";//add appropriate info
-            lblPests.Text = "Common pests: " + croplist[counter].Pests.ToString();
+            lblCName.Text = "Crop name: " + croplist[0].CropName.ToString();
+            lblSTime.Text = "Sow time: " + croplist[0].SowTime.ToString();
+            lblHTime.Text = "Harvest time: " + croplist[0].HarvestTime.ToString() + "";//add appropriate info
+            lblIAmmount.Text = "Irrigation amount needed: " + croplist[0].IrrigationAmount.ToString() + "";//add appropriate info
+            lblPests.Text = "Common pests: " + croplist[0].Pests.ToString();
         }
 
         private async void Button_Clicked_Next(object sender, EventArgs e)
         {
-            counter++;
-            if (counter > croplist.Count)
+            if (croplist.Count > 0) //if the crop list has multiple crop selections, allows the user to loop through the suggestions
             {
-                counter = 0;
-            }
+                counter++;
+                if (counter > croplist.Count)
+                {
+                    counter = 0;
+                }
 
-            Img = croplist[counter].CropImage;
-            lblCName.Text = "Crop name: " + croplist[counter].CropName.ToString();
-            lblSTime.Text = "Sow time: " + croplist[counter].SowTime.ToString();
-            lblHTime.Text = "Harvest time: " + croplist[counter].HarvestTime.ToString() + "";//add appropriate info
-            lblIAmmount.Text = "Irrigation amount needed: " + croplist[counter].IrrigationAmount.ToString() + "";//add appropriate info
-            lblPests.Text = "Common pests: " + croplist[counter].Pests.ToString();
+                Img = croplist[counter].CropImage;
+                lblCName.Text = "Crop name: " + croplist[counter].CropName.ToString();
+                lblSTime.Text = "Sow time: " + croplist[counter].SowTime.ToString();
+                lblHTime.Text = "Harvest time: " + croplist[counter].HarvestTime.ToString() + "";//add appropriate info
+                lblIAmmount.Text = "Irrigation amount needed: " + croplist[counter].IrrigationAmount.ToString() + "";//add appropriate info
+                lblPests.Text = "Common pests: " + croplist[counter].Pests.ToString();
+            }
+            else//shows a message to user that only one crop suggestion could be found for their location
+            {
+                await DisplayAlert("Alert", "There are no additional crop suggestions", "OK");
+            }
+            
         }
 
         private async void Button_Clicked_Previous(object sender, EventArgs e)
         {
-            counter--;
-            if (counter < 0)
+            if (croplist.Count > 0) //if the crop list has multiple crop selections, allows the user to loop through the suggestions
             {
-                counter = croplist.Count - 1;
-            }
+                counter--;
+                if (counter < 0)
+                {
+                    counter = croplist.Count - 1;
+                }
 
-            Img = croplist[counter].CropImage;
-            lblCName.Text = "Crop name: " + croplist[counter].CropName.ToString();
-            lblSTime.Text = "Sow time: " + croplist[counter].SowTime.ToString();
-            lblHTime.Text = "Harvest time: " + croplist[counter].HarvestTime.ToString() + "";//add appropriate info
-            lblIAmmount.Text = "Irrigation amount needed: " + croplist[counter].IrrigationAmount.ToString() + "";//add appropriate info
-            lblPests.Text = "Common pests: " + croplist[counter].Pests.ToString();
+                Img = croplist[counter].CropImage;
+                lblCName.Text = "Crop name: " + croplist[counter].CropName.ToString();
+                lblSTime.Text = "Sow time: " + croplist[counter].SowTime.ToString();
+                lblHTime.Text = "Harvest time: " + croplist[counter].HarvestTime.ToString() + "";//add appropriate info
+                lblIAmmount.Text = "Irrigation amount needed: " + croplist[counter].IrrigationAmount.ToString() + "";//add appropriate info
+                lblPests.Text = "Common pests: " + croplist[counter].Pests.ToString();
+            }
+            else//shows a message to user that only one crop suggestion could be found for their location
+            {
+                await DisplayAlert("Alert", "There are no additional crop suggestions", "OK");
+            }
+            
         }
     }
 }
