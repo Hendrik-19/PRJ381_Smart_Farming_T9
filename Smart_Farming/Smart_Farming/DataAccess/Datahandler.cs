c62e80ebcffbe8b8ceca9a0e1c271f4b65d8afc6
Hendrik-19 <576326@student.belgiumcampus.ac.za>
2021-11-08 10:40:13 +0200

    Added Sqlite nuget package and added a static constant class

--- Smart_Farming/Smart_Farming/DataAccess/Datahandler.cs
+++ Smart_Farming/Smart_Farming/DataAccess/Datahandler.cs
@@ -10,32 +10,7 @@ namespace Smart_Farming.DataAccess
     public class Datahandler
     {
         #region Database
-        private string connection = @"Data Source=.; Initial Catalog = SmartFarmingDB; Integrated Security = SSPI"; //Database connection string
-
-        SqlConnection conn; // used establish a connection to the SQL server database
-        SqlDataAdapter adapter; // used to convert the data from the database to a datatable
-
-        public DataTable getData(string query)
-        {
-            DataTable table = new DataTable(); // datatable to store the data from the database
-            conn = new SqlConnection(connection); // opens the connection to the database
-
-            try
-            {
-                adapter = new SqlDataAdapter(query, conn); // will try to execute the query 
-                adapter.Fill(table); // will fill the table with the data received from the database if the execution was successful
-            }
-            catch (Exception e)
-            {
-                App.Current.MainPage.DisplayAlert("Alert", "Could not find results... :"+e.Message, "OK"); // Error message that will display on the main form, Not sure what the App.Current.MainPage part does.
-            }
-            finally
-            {
-                conn.Close(); // will ultimatly close the connection to the database once everything has been done
-            }
-
-            return table;
-        }
+        
         #endregion
     }
 }
