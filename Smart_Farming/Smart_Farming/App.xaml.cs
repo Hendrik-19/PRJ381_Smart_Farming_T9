9406221f4ee079acb59d7da63a11dda27aadcbca
Juan-Pierre Prinsloo <576598@student.belgiumcampus.ac.za>
2021-11-09 15:51:08 +0200

    Added database creation/connection to App.xaml.cs

--- Smart_Farming/Smart_Farming/App.xaml.cs
+++ Smart_Farming/Smart_Farming/App.xaml.cs
@@ -1,11 +1,28 @@
 ﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using System.IO;
+using Smart_Farming.DataAccess;
 
 namespace Smart_Farming
 {
     public partial class App : Application
     {
+        static SmartFarmingBD database;
+
+        //Creating database as singleton
+        public static SmartFarmingBD Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new SmartFarmingBD(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "SmartFarmingDB.db3"));
+                }
+                return database;
+            }
+        }
+
         public App()
         {
             InitializeComponent();
