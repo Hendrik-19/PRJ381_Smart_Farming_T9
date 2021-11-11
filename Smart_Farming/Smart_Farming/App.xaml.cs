4f3b39abc0164d65c3b39b9b66982f0081302a9f
Hendrik-19 <576326@student.belgiumcampus.ac.za>
2021-11-11 15:15:40 +0200

    added database things

--- Smart_Farming/Smart_Farming/App.xaml.cs
+++ Smart_Farming/Smart_Farming/App.xaml.cs
@@ -6,6 +6,20 @@ namespace Smart_Farming
 {
     public partial class App : Application
     {
+        static SmartFarmingBD database;
+
+        //Creating database as singleton
+        public static SmartFarmingBD Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new SmartFarmingBD(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "SmartFarmingDB.db3"));
+                }
+                return database;
+            }
+        }
         public App()
         {
             InitializeComponent();
